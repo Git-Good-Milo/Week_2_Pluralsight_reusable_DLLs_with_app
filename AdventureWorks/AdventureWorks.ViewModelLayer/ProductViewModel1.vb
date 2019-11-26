4f7273ec214ce705f8dd0590ffef31307bc357b8
Miles <milotheengineer@gmail.com>
2019-11-26 14:55:24 +0000

    Initial commit with basic first aplication made!

---
+++ AdventureWorks/AdventureWorks.ViewModelLayer/ProductViewModel1.vb
@@ -0,0 +1,33 @@
+﻿Imports AdventureWorks.EntityLayer
+Imports Common.Library
+
+Public Class ProductViewModel1
+    Inherits ViewModelBase
+
+    Sub New()
+        LoadProduct(680)
+    End Sub
+
+    Public Property Entity As Product
+
+    Function LoadProduct(ByVal productID As Integer) As Product
+        Return LoadProduct(productID, Nothing)
+    End Function
+
+    Function LoadProduct(ByVal produtID As Integer, ByVal startingFilePath As String) As Product
+
+        ' Hard code an entity
+        Entity = New Product() With {
+            .ProductID = 680,
+            .Name = "l Rad Frame - black, 58",
+            .ProductNumber = "FR-R92B-58",
+            .Color = "Black",
+            .Size = "58",
+            .Weight = 1016.04D,
+            .StandardCost = 1059.31D
+        }
+
+        Return Entity
+    End Function
+
+End Class
